719be365dc484b59e1be75265ae009779409c320
Hsuan <lemon861026@gmail.com>
2021-05-24 01:28:04 +0800

    test step8

--- dist/practice.js
+++ dist/practice.js
@@ -1,15 +1,11 @@
 $(document).ready(function() {
     $('#ajax-form button[type="submit"]').click((event) => {
       event.preventDefault()
-      // Step 9 and step 10 code goes here
-  
-      $.get('./step5', {
+      $.get('/step5', {
         fname: $('#ajax-form input[name=fname]').val(),
         lname: $('#ajax-form input[name=lname]').val(),
       }, (data) => {
         $('#ajax-output').html(data);
       })
-  
-      // Step 11 code goes here
     })
   });
