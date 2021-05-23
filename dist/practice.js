8948fb12b1df990356534b30c3744a3424f649b9
Hsuan <lemon861026@gmail.com>
2021-05-24 01:09:24 +0800

    test step8

--- dist/practice.js
+++ dist/practice.js
@@ -1,11 +1,10 @@
-$('#ajax-form button[type="submit"]').click((event)=>{
-    console.log($('#ajax-form input[name=fname]').val());
-    console.log($('#ajax-form input[name=lname]').val());
-})
-
-// $.post('../step5', {
-//     fname: $('#ajax-form input[name=fname]').val(),
-//     lname: $('#ajax-form input[name=lname]').val(),
-//   },(data)=>{
-//     $('#ajax-output').html(data)
-//   })
+$(document).ready(function() {
+    $('#ajax-form button[type="submit"]').click((event) => {
+      event.preventDefault()
+      // Step 9 and step 10 code goes here
+  
+      $.get('./step5')
+  
+      // Step 11 code goes here
+    })
+  });
