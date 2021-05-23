5398109a7325b47eb74e5d8075240366baea49e1
Hsuan <lemon861026@gmail.com>
2021-05-24 03:16:50 +0800

    list all students

--- dist/practice.js
+++ dist/practice.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
     $('#list button[type="submit"]').click((event) => {
         event.preventDefault()
         $.get('/list_all',(data)=>{
-            $('#list-output').html();
+            $('#list-output').html('');
             student_all = data;
             for(i=0;i<Object.keys(student_all).length;i++){
                 $('#list-output').append(`"${Object.keys(student_all)[i]}":"${student_all[Object.keys(student_all)[i]]}"<br>`);
