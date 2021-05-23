18d98ff55d55599206f3511715a30ec09af1cdf7
Hsuan <lemon861026@gmail.com>
2021-05-24 00:54:00 +0800

    test step8

--- dist/practice.js
+++ dist/practice.js
@@ -1,6 +1,8 @@
-$.get('../step5', {
-    fname: $('#ajax-form input[name=fname]').val(),
-    lname: $('#ajax-form input[name=lname]').val(),
-  },(data)=>{
-    $('#ajax-output').html(data)
-  })
+console.log($('#ajax-form input[name=fname]').val());
+console.log($('#ajax-form input[name=lname]').val());
+// $.post('../step5', {
+//     fname: $('#ajax-form input[name=fname]').val(),
+//     lname: $('#ajax-form input[name=lname]').val(),
+//   },(data)=>{
+//     $('#ajax-output').html(data)
+//   })
