667bada9e1e3d1bf651a7b38d5ce2349d350ae48
Hsuan <lemon861026@gmail.com>
2021-05-23 23:13:37 +0800

    test step7

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const fs = require("fs");
+const bodyParser = require('body-parser');
 const port = 2048;
 
 app.use("/",express.static(__dirname));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 app.listen(port, () => {
     console.log(`listening on port: ${port}`)
   })
@@ -13,5 +16,9 @@ app.get('/step1', (req, res) => {
   })
 
 app.get('/step5', (req, res) => {
-res.send(`Hello, ${req.query.fname} ${req.query.lname}`)
-})
+    res.send(`Hello, ${req.query.fname} ${req.query.lname}`)
+  })
+
+app.post('/step7', (req, res) => {
+    res.send(`Hello, ${req.body.fname} ${req.body.lname}`)
+  })
