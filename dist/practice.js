6c0eb7c84b612ef7a201ea512b2cccd6ed2b52e3
Hsuan <lemon861026@gmail.com>
2021-05-24 11:56:27 +0800

    delete unnecessary code

--- dist/practice.js
+++ dist/practice.js
@@ -1,14 +1,5 @@
 var student_all;
 $(document).ready(function() {
-    // $('#ajax-form button[type="submit"]').click((event) => {
-    //   event.preventDefault()
-    //   $.get('/step5', {
-    //     fname: $('#ajax-form input[name=fname]').val(),
-    //     lname: $('#ajax-form input[name=lname]').val(),
-    //   }, (data) => {
-    //     $('#ajax-output').html(data);
-    //   })
-    // })
     $('#list button[type="submit"]').click((event) => {
         event.preventDefault()
         $.get('/list_all',(data)=>{
@@ -19,12 +10,6 @@ $(document).ready(function() {
             }
             
         })
-        // $.get('/list_all', {
-        //   fname: $('#ajax-form input[name=fname]').val(),
-        //   lname: $('#ajax-form input[name=lname]').val(),
-        // }, (data) => {
-        //   $('#ajax-output').html(data);
-        // })
     })
     $('#search button[type="submit"]').click((event) => {
         event.preventDefault()
--- server.js
+++ server.js
@@ -15,21 +15,9 @@ let student_path = './dist/student.json';
 let student_rawdata = fs.readFileSync(student_path);
 let student = JSON.parse(student_rawdata);
 
-// app.get('/step1', (req, res) => {
-//     res.send('hello world')
-//   })
-
-// app.get('/step5', (req, res) => {
-//     res.send(`Hello, ${req.query.fname} ${req.query.lname}`)
-//   })
-
-// app.post('/step7', (req, res) => {
-//     res.send(`Hello, ${req.body.fname} ${req.body.lname}`)
-//   })
 
 app.get('/list_all',(req,res)=>{
     res.send(student);
-    // console.log(student);
 })
 app.get('/search_name',(req,res)=>{
   if(student[req.query.ID]!=undefined){
@@ -49,7 +37,6 @@ app.get('/add_student',(req,res)=>{
   })
 })
 app.get('/delete_student',(req,res)=>{
-  // res.send(student[req.query.ID])
   if(student[req.query.ID]==undefined){
     res.send("Fail!!No result");
   }else{
@@ -61,6 +48,5 @@ app.get('/delete_student',(req,res)=>{
         res.send('Success');
       }
     })
-    // res.send(`${req.query.ID} ${student[req.query.ID]}`);
   }
 })
